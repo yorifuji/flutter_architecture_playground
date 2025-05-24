63033d8120cd0e8cab00e639cd64e9dc7e4fbd64
yorifuji <yorifuji@gmail.com>
2025-05-24 18:25:03 +0900

    page

---
+++ lib/page2/page2.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_architecture_playground/page1/view_model.dart';
+
+class Page2 extends ConsumerWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncData = ref.watch(viewModelProvider);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page')),
+      body: Center(
+        child: asyncData.when(
+          data:
+              (data) => Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Timestamp: ${data['timestamp']}',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  Text(
+                    'Message: ${data['message']}',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed:
+                        () =>
+                            ref.read(viewModelProvider.notifier).refreshData(),
+                    child: const Text('Refresh Data'),
+                  ),
+                ],
+              ),
+          loading: () => const CircularProgressIndicator(),
+          error: (error, stack) => Text('Error: $error'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/repository/dummy_repository.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dummy_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dummyRepositoryHash() => r'f39f74bac732393e101636c53a3d0fd1ce203fb6';
+
+/// See also [dummyRepository].
+@ProviderFor(dummyRepository)
+final dummyRepositoryProvider = AutoDisposeProvider<DummyRepository>.internal(
+  dummyRepository,
+  name: r'dummyRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$dummyRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef DummyRepositoryRef = AutoDisposeProviderRef<DummyRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
